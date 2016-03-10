d640cddcd1cee858bb42406c12bb08e99059bfee
felix <mljmq@126.com>
2016-03-10 11:05:45 +0800

    發貨及下架

--- app/controllers/barcodes_controller.rb
+++ app/controllers/barcodes_controller.rb
@@ -153,6 +153,76 @@ class BarcodesController < ApplicationController
     render json: response
   end
 
+  def out
+
+  end
+
+  def out_scan
+    rows    = []
+    barcode = Barcode.find_by_uuid params[:barcode]
+    if barcode.present?
+      uuid = barcode.parent_id.size == 32 ? barcode.parent_id : barcode.uuid
+      sql  = "
+          select a.uuid,a.seq,a.name,a.menge,b.matnr,b.charg
+            from barcode a join stock_master b on b.uuid = a.stock_master_id
+            where a.parent_id = ? or a.uuid = ?
+        "
+      rows = Barcode.find_by_sql [sql, uuid, uuid]
+    end
+    render json: rows
+  end
+
+  def out_takeaway
+    barcodes = Barcode.includes(:stock_master).joins(:stock_master)
+                   .where(uuid: params[:uuid].split(','))
+
+    response     = {
+        barcode_uuid: 'success'
+    }
+    barcode_uuid = ''
+    StockTran.transaction do
+      begin
+        barcodes.each { |barcode|
+          barcode_uuid = barcode.uuid
+          StockTran.create({
+                               master_id:   barcode.stock_master_id,
+                               barcode_id:  barcode.uuid,
+                               lgort_old:   barcode.storage.blank? ? barcode.lgort : barcode.storage,
+                               lgort:       params[:storage],
+                               status:      barcode.status,
+                               menge:       barcode.menge,
+                               vbeln:       '',
+                               posnr:       '',
+                               meins:       barcode.stock_master.meins,
+                               mjahr:       barcode.stock_master.mjahr,
+                               mblnr:       barcode.stock_master.mblnr,
+                               zeile:       barcode.stock_master.zeile,
+                               aufnr:       barcode.stock_master.aufnr,
+                               prdln:       barcode.stock_master.prdln,
+                               ebeln:       barcode.stock_master.ebeln,
+                               ebelp:       barcode.stock_master.ebelp,
+                               mtype:       'in_putway',
+                               created_by:  current_user.email,
+                               created_ip:  request.ip,
+                               created_mac: '',
+                               updated_by:  current_user.email,
+                               updated_ip:  request.ip,
+                               updated_mac: '',
+                           })
+          barcode.status  = 'putaway'
+          barcode.storage = params[:storage]
+          barcode.save
+        }
+      rescue Exception => e
+        response = {
+            barcode_uuid:  barcode_uuid,
+            error_message: e.message
+        }
+      end
+    end
+    render json: response
+  end
+
   def repeat_printer
     if params[:barcode].present?
       @barcode = Barcode.find_by_uuid(params[:barcode])
