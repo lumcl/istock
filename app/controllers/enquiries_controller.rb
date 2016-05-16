0700b5ad5e15da8271fa814830399e1805a2408d
felix <mljmq@126.com>
2016-05-16 09:54:52 +0800

    enquiries_controller

--- app/controllers/enquiries_controller.rb
+++ app/controllers/enquiries_controller.rb
@@ -52,6 +52,37 @@ class EnquiriesController < ApplicationController
     end
   end
 
+  def mblnr_onhand
+    if params[:mblnr]
+      sql       = "
+        select a.matnr,a.maktx,a.matkl,a.charg,a.werks,a.meins,a.aufnr,a.datecode,a.budat,
+               b.uuid,b.name,b.menge,a.werks ||'-'|| nvl(b.storage,b.lgort)storage,b.status,b.seq
+          from stock_master a
+            join barcode b on b.stock_master_id = a.uuid and b.status <> 'takeaway'
+            where a.mblnr = ? and a.werks like '%#{params[:werks].upcase}%'
+      "
+      @barcodes = StockMaster.find_by_sql [sql, params[:mblnr].upcase]
+
+      menge      = 0
+      pallet_cnt = 0
+      box_cnt    = 0
+      storages   = {}
+      @barcodes.each { |barcode|
+        menge += barcode.menge
+        barcode.name == 'box' ? box_cnt += 1 : pallet_cnt += 1
+        storages[barcode.storage] = [0,0,0] unless storages.key?(barcode.storage)
+        storages[barcode.storage][0] += barcode.menge
+        barcode.name == 'box' ? storages[barcode.storage][1] += 1 : storages[barcode.storage][2] += 1
+      }
+      @htable = {
+          menge:      menge,
+          pallet_cnt: pallet_cnt,
+          box_cnt:    box_cnt,
+          storages:   storages
+      }
+    end
+  end
+
   def not_putaway
     if params[:werks]
       sql       = "
